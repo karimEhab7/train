e2f4afb10a3353dd0a669877b4da11ae4c29c011
karimEhab7 <karimehab97@gmail.com>
2019-09-22 00:06:43 +0200

    first commit

---
+++ SimulationGP/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimulationGP.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            var movie = new Models.Movie(){ Name="shrek!"};
+
+            return View(movie);
+        }
+    }
+}
